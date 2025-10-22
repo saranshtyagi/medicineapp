928bcea46477f8ede2341a6c8270f974a549f454
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-10-22 20:27:15 +0530

    feat: Implement Header component with delivery information and icons

---
+++ src/components/Header.tsx
@@ -0,0 +1,51 @@
+import {StyleSheet, Text, View, Pressable} from 'react-native';
+import React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {MainStackParamList} from '../navigation/Routes';
+import Ionicons from '@react-native-vector-icons/ionicons';
+
+const Header = () => {
+  const nav = useNavigation<NativeStackNavigationProp<MainStackParamList>>();
+  return (
+    <View className="px-4 pb-4 py-2 rounded-b-2x;">
+      <Text className="text-white text-sm">Delivery starting in</Text>
+      <View className='flex-row items-end justify-between mt-1'>
+        <View>
+          <Text className="text-white text-3xl font-extrabold">
+            15 minutes⚡
+          </Text>
+          <View className="flex-row items-center mt-1">
+            <Text className="text-white font-medium">201307 - Noida</Text>
+            <Ionicons
+              name="chevron-down-outline"
+              size={16}
+              color={'#fff'}
+              style={{marginLeft: 6}}
+            />
+          </View>
+        </View>
+        <View className='flex-row items-center gap-2'>
+          <Pressable>
+            <Ionicons
+              name="heart-outline"
+              size={22}
+              color={'#fff'}
+            />
+          </Pressable>
+          <Pressable>
+            <Ionicons
+              name="person-outline"
+              size={22}
+              color={'#fff'}
+            />
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({});
