821f55254a2a0c6a65bf55b2a7b9ae5cf2b896f5
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-11-06 23:04:24 +0530

    feat: Add CartScreen to MainNavigator for navigation

--- src/navigation/navigators/MainNavigator.tsx
+++ src/navigation/navigators/MainNavigator.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import MainTabNavigator from './MainTabNavigator';
 import CategoryScreen from '../../screens/CategoryScreen';
+import CartScreen from '../../screens/CartScreen';
 
 const Stack = createNativeStackNavigator(); 
 
@@ -11,6 +12,7 @@ const MainNavigator = () => {
     <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="MainTabs" component={MainTabNavigator} />
         <Stack.Screen name="Category" component={CategoryScreen} />
+        <Stack.Screen name="Cart" component={CartScreen} />
     </Stack.Navigator>
   )
 }
