e3590332b886bebb22a1e42aecbd3f91ae3d4754
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-11-08 22:18:55 +0530

    feat: Add SplashScreen component to RootNavigator for loading state indication

--- src/navigation/navigators/RootNavigator.tsx
+++ src/navigation/navigators/RootNavigator.tsx
@@ -1,21 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { RootRoutes } from '../Routes';
 import MainNavigator from './MainNavigator';
 import AuthNavigator from './AuthNavigator';
 import { useAuthStore } from '../../store/useAuthStore';
+import Ionicons from '@react-native-vector-icons/ionicons';
 
 const Stack = createNativeStackNavigator();
 
+const SplashScreen = () => {
+  return (
+    <SafeAreaView className='flex-1 bg-green-700 justify-center items-center'>
+      <Image />
+      <View className='mt-10 pb-8 items-center'>
+        <View className='flex-row items-center gap-2'>
+          <Ionicons name='medkit-outline' size={26} color="#7c3aed" />
+          <Text className='text-2xl font-extrabold text-white'>Bringing the Pharmacy to You</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
 const RootNavigator = () => {
-    const {isAuthenticated} = useAuthStore();
+    const {isAuthenticated, token} = useAuthStore();
     const [isLoading, setIsLoading] = useState(true);
 
     useAuthStore.persist.onFinishHydration(() => {
       console.log('Auth state rehydrated', {isAuthenticated, token}); 
       setIsLoading(false);
-    })
+    }); 
+
+    if(isLoading) {
+      return <SplashScreen />
+    }
+
   return (
     <Stack.Navigator screenOptions={{headerShown:false}}>
         {isAuthenticated ? (
